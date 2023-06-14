84c191fedaa654baa5f7fe27dbd70f8195c8bb8e
hildelio <hildelio@gmail.com>
2023-06-14 10:05:03 -0300

    Req 02 User Model

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const userTable = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    tableName: 'users',
+    timestamps: false,
+    underscored: true
+  });
+  return userTable;
+};
