18c884bffd88243a1d9341ec78991f3bdf101ba7
hildelio <hildelio@gmail.com>
2023-06-15 19:27:16 -0300

    Req 07 Model Category

---
+++ src/models/Category.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const userTable = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    name: DataTypes.STRING,
+  }, {
+    tableName: 'categories',
+    timestamps: false,
+    underscored: true,
+  });
+  return userTable;
+};
